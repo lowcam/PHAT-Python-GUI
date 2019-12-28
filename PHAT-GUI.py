010fbf83419c596aaaae15a7eceddd61300769ce
Lorne Cammack <lowcam.socailvideo@gmail.com>
2019-12-27 22:07:43 -0600

    This is the initial gui code made with appJar.

--- PHAT-GUI.py
+++ PHAT-GUI.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # PHAT  - Password Hashing Algorithm Tool
 # GUI Python Version
-# v 0.1
+# v 0.2
 #
 # The purpose of this tool is to let an individual enter text and have a hashed
 # output to use as the password to the site or program. Initially the program
@@ -36,183 +36,108 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------------------
 
-
+from appJar import gui
 import codecs
 import hashlib
 import base58
 from tkinter import Tk
 
-#Print license at the beginning of the file
-def printLicense():
-    print ("")
-    print ("PHAT Copyright (C) 2019 Lorne Cammack")
-    print ("This program comes with ABSOLUTELY NO WARRANTY;")
-    print ("This is free software, and you are welcome to redistribute it")
-    print ("under certain conditions. See https://www.gnu.org/licenses/ for more details.")
-    print ("")
+# create a GUI variable called app
+app = gui("PHAT v0.2", "500x500")
+app.setBg("white")
+app.setFont(10)
 
-# This section has the user choose between 3 SHA lengths: 256, 384, and 512
-def selectSHA():
-    i = 1
-    while (i == 1):
-        shainput = input ('What SHA value would you like? (256, 384, or 512) ')
-        global valuesha
-        try:
-            testVal = int(shainput)
-        except ValueError:
-            print("Incorrect value entered. Please try again.")
-            i=1
+def press(button):
+    if button == "Exit":
+        app.stop()
+    elif button == "Copy Result":
+        copytext = app.getTextArea("calcResultMessage")
+        r = Tk()
+        r.withdraw()
+        r.clipboard_clear()
+        r.clipboard_append(copytext)
+        r.update()
+        r.destroy()
+    else:
+        inputText = app.getEntry("TextString")
+        shavalue = app.getOptionBox("SHA")
+        valuesha = int(shavalue)
+        numsystext = app.getOptionBox("NumberSystem")
+        numsysvalue = 1
+        if numsystext == "Base64":
+            numsysvalue = 2
+        elif numsystext == "Base58":
+            numsysvalue = 3
         else:
-            valuesha = int(shainput)
-            if valuesha == 256:
-                i = 2
-            elif valuesha == 384:
-                i = 2
-            elif valuesha == 512:
-                i = 2
-            else:
-                print ("Incorrect value entered. Please try again.")
-                i=1
-
-# This section deals with selecting an output number system between
-# Hexidecimal, Base 64, and Base 58.
-def OutputNumberSystem():
-    i=1
-    while (i==1):
-        numsysvalue = input ('Choose output number system: 1. Hex; 2. Base64; 3. Base58  ')
-        global valuenumsys
-        try:
-            testVal = int(numsysvalue)
-        except:
-            print("Incorrect value entered. Please try again.")
-            i=1
-        else:
-            valuenumsys = int(numsysvalue)
-            if valuenumsys == 1:
-                i = 2
-            elif valuenumsys == 2:
-                i = 2
-            elif valuenumsys == 3:
-                i = 2
-            else:
-                print ("Incorrect value entered. Please try again.")
-                i=1
-
-# This section lets a user decide how many digits they want in the final output
-def numdigfinal():
-    i = 1
-    while (i == 1):
-        global finaldig
-        digfinalyn = input ('Would you like to restrict the number of digits in the output? (Y/N) ')
-        if digfinalyn == 'Y' or digfinalyn == 'y':
-            digfinal = input ('How many digits would you like? (1-128) ')
-            try:
-                val = int(digfinal)
-            except:
-                print("Incorrect value entered. Please try again.")
-                i=1
-            else:
-                finaldig = int(digfinal)
-                if finaldig < 1 or finaldig > 128:
-                    print ("This value is not acceptable")
-                    i=1
-                else:
-                    i=2
-        elif digfinalyn == 'N' or digfinalyn == 'n':
+            numsysvalue = 1
+        valuenumsys = int(numsysvalue)
+        finaldig = 0
+        digfinalyn = app.getRadioButton("restrictDigitsYN")
+        if digfinalyn == "No":
             finaldig = 0
-            i=2
         else:
-            print ("That is an incorrect selection.")
-            i = 1
-
-# This section gets the values ready to print. For Base64 and Base58 it
-# decodes the output from Hex, which it is in when it is sent, and then
-# reencodes it into the numbering system choosen in OutputNumberSystem().
-def outputPrint(hexhashvalue):
-    global printreturn
-    printreturn=0
-    if valuenumsys == 1:
-        printreturn = hexhashvalue
-    elif valuenumsys == 2:
-        printreturn = codecs.encode(codecs.decode(hexhashvalue, 'hex'), 'base64').decode()
-# The base64 return was putting a line return at the 76 character mark, which
-# when copying into a password did not help. This code rectifies that. Also
-# had to add the rstrip() so that a new line wouldn't be added after
-# concatenating the two parts.
-        printreturnLen = len(printreturn)
-        if printreturnLen > 76:
-            printreturnSide1 = printreturn[:76]
-            printreturnEndCalc = printreturnLen - 77
-            printreturnSide2 = printreturn[-printreturnEndCalc:]
-            printreturn = printreturnSide1 + printreturnSide2
-            printreturn = printreturn.rstrip()
-    else:
-        printreturn = base58.b58encode(codecs.decode(hexhashvalue, 'hex'))
-
-# This section gets everything ready for the output
-def finalprint (hexhashvalue):
-
-    lenhash = len(hexhashvalue)
-    global copyreturn
-    copyreturn = printreturn
-    if finaldig == 0 or finaldig >= lenhash:
-        if valuenumsys == 1:
-            #printreturn = hexhashvalue
-            print ("SHA", valuesha, "sum in hex: ")
-            print (printreturn)
-        elif valuenumsys == 2:
-            #printreturn = codecs.encode(codecs.decode(hexhashvalue, 'hex'), 'base64').decode()
-            print ("SHA", valuesha, "sum in base64: ")
-            print (printreturn)
+            digfinal = app.getScale("restrictDigitScale")
+            finaldig = int(digfinal)
+        hashText = bytes(inputText, "ascii")
+        outputText = 0
+        if valuesha == 256:
+            outputText = hashlib.sha256 (hashText).hexdigest()
+        elif valuesha == 384:
+            outputText = hashlib.sha384 (hashText).hexdigest()
         else:
-            #printreturn = base58.b58encode(codecs.decode(hexhashvalue, 'hex'))
-            print ("SHA", valuesha, "sum in base58: ")
-            print (printreturn)
-    else:
-        if valuenumsys == 1:
-            #printreturn = hexhashvalue
-            print ("SHA", valuesha, "sum in hex: ")
-            print (printreturn[:finaldig])
-            copyreturn = printreturn[:finaldig]
-        elif valuenumsys == 2:
-            #printreturn = codecs.encode(codecs.decode(hexhashvalue, 'hex'), 'base64').decode()
-            print ("SHA", valuesha, "sum in base64: ")
-            print (printreturn[:finaldig])
-            copyreturn = printreturn[:finaldig]
+            outputText = hashlib.sha512 (hashText).hexdigest()
+        outputPrint = 0
+        if numsysvalue == 1:
+            outputPrint = outputText
+        elif numsysvalue == 2:
+            outputPrint = codecs.encode(codecs.decode(outputText, 'hex'), 'base64').decode()
+            printreturnLen = len(outputPrint)
+            if printreturnLen > 76:
+                printreturnSide1 = outputPrint[:76]
+                printreturnEndCalc = printreturnLen - 77
+                printreturnSide2 = outputPrint[-printreturnEndCalc:]
+                outputPrint = printreturnSide1 + printreturnSide2
         else:
-            #printreturn = base58.b58encode(codecs.decode(hexhashvalue, 'hex'))
-            print ("SHA", valuesha, "sum in base58: ")
-            print (printreturn[:finaldig])
-            copyreturn = printreturn[:finaldig]
+            outputPrint = base58.b58encode(codecs.decode(outputText, 'hex'))
+        lenhash = len(outputText)
+        if finaldig == 0 or finaldig >=lenhash:
+            app.clearTextArea("calcResultMessage")
+            app.setTextArea("calcResultMessage", outputPrint)
+        else:
+            OutputPrintConcat = outputPrint[:finaldig]
+            app.clearTextArea("calcResultMessage")
+            app.setTextArea("calcResultMessage", OutputPrintConcat)
 
-printLicense()
-selectSHA()
-OutputNumberSystem()
-numdigfinal()
-# Input from user.
-inputText = input ('Enter value: ')
-# Take the input text and convert it into byte format.
-hashText = bytes(inputText, "ascii")
+# add & configure widgets - widgets get a name, to help referencing them later
+app.addLabel("title", "PHAT v0.2")
+app.setLabelBg("title", "red")
+app.addLabelEntry("TextString")
+app.addLabelOptionBox("SHA", ["256", "384", "512"])
+app.addLabelOptionBox("NumberSystem", ["Hex", "Base64", "Base58"])
+app.addLabel("restrictDigits", "Restrict the Number of Output Digits?")
+app.addRadioButton("restrictDigitsYN", "Yes")
+app.addRadioButton("restrictDigitsYN", "No")
+app.setRadioButton("restrictDigitsYN", "No", callFunction=False)
+app.addLabel("restrictDigitScaleText", "Number of Output Digits")
+app.addScale("restrictDigitScale")
+app.setScaleRange("restrictDigitScale", 1, 128, curr=128)
+app.showScaleValue("restrictDigitScale", show=True)
+app.addLabel("calcResult", "Calc Result")
+app.addTextArea("calcResultMessage")
+app.setTextArea("calcResultMessage", "Push the Calc Button to See the Result")
+#app.setEntryDefault("CalcResult", "Push the Calc Button to See the Result")
+app.addButtons(["Calculate", "Copy Result", "Exit"], press)
+app.setFocus("TextString")
 
-# This next section hashes the hashText into the correct SHA type as selected
-# above
-outputText = 0
-if valuesha == 256:
-    outputText = hashlib.sha256 (hashText).hexdigest()
-elif valuesha == 384:
-    outputText = hashlib.sha384 (hashText).hexdigest()
-else:
-    outputText = hashlib.sha512 (hashText).hexdigest()
 
-outputPrint(outputText)
-finalprint(outputText)
+# start the GUI
+app.go()
 
-# code to copy output to clipboard_clear
-r = Tk()
-r.withdraw()
-r.clipboard_clear()
-r.clipboard_append(copyreturn)
-r.update()
-r.destroy()
-
-exitText = input ('Hash Copied to Clipboard. Paste before pressing a key to continue')
+#Print license at the beginning of the file
+#def printLicense():
+#    print ("")
+#    print ("PHAT Copyright (C) 2019 Lorne Cammack")
+#    print ("This program comes with ABSOLUTELY NO WARRANTY;")
+#    print ("This is free software, and you are welcome to redistribute it")
+#    print ("under certain conditions. See https://www.gnu.org/licenses/ for more details.")
+#    print ("")
